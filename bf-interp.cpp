7f7fd4e0517cc1b257ee28cd477600ef46662126
Mohmed Jasim P <mohd.jas@gmail.com>
2014-01-20 22:21:08 +0300

    Completed interpreter. Now takes input through file or string on the command-line.

--- bf-interp.cpp
+++ bf-interp.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
+#include <fstream>
 #include <cstring>
 #include <cstdio>
+#include <stack>
 using namespace std;
-/*
-List of Brainfuck symbols:
->  - increment data pointer
-<  - decrement data pointer
-+  - increment byte at data pointer
--  - decrement byte at data pointer
-.  - output byte at data pointer
-,  - accept one byte of input
-[  - if byte at data pointer is zero, skip to next command after matching ], else next command after [
-]  - if byte at data pointer is zero, next command, else next command after previous [
-*/
 
 class Brainfuck {
     char* instructionTape;
     char* dataTape;
     unsigned int dataPointer;
     unsigned int instrPointer;
+    stack<unsigned int> brackets;
   public:
-    Brainfuck(const char* tape){
-      instructionTape = new char[strlen(tape)];
-      strcpy(instructionTape, tape);
+    Brainfuck(ifstream& tape){
+      tape.seekg(0, tape.end);
+      int length = tape.tellg();
+      tape.seekg(0, tape.beg); //reset pointer to beginning of file
+      instructionTape = new char[length];
+      tape.read(instructionTape, length);
+      
       dataTape = new char[2000];
       memset(dataTape, 0, sizeof(char)*2000);
-      
+      dataPointer = 0;
+      instrPointer = 0;
+    }
+    
+    Brainfuck(const char* tape){
+      instructionTape = new char[strlen(tape)+1];
+      strcpy(instructionTape, tape);
+
+      dataTape = new char[2000];
+      memset(dataTape, 0, sizeof(char)*2000);
       dataPointer = 0;
       instrPointer = 0;
     }
     void interpret(char);
     void run();
+    void run(ifstream&);
 };
   
-
 void Brainfuck::interpret(char symbol){
   switch(symbol){
     case '>':
@@ -55,28 +60,59 @@ void Brainfuck::interpret(char symbol){
       dataTape[dataPointer] = getchar();
       break;
     case '[':        
-      //TODO
+      brackets.push(instrPointer);
+      if (dataTape[dataPointer] == 0){
+      /*
+        Push the opening brackets onto the stack and
+        if a closing bracket is encountered, pop it.
+        This ensures that the current code block is 
+        skipped entirely.        
+      */
+        while (!brackets.empty()){
+          instrPointer++;
+          if (instructionTape[instrPointer] == ']')
+            brackets.pop();
+          else if (instructionTape[instrPointer] == '[')
+            brackets.push(instrPointer);
+        }
+      }
       break;
     case ']':
-      //TODO
+      /*
+        We subtract 1 from the index because we
+        increment it after each symbol is read.
+      */
+      if (dataTape[dataPointer] != 0){
+        instrPointer = brackets.top() - 1;
+      }
+      brackets.pop();
       break;
     default: 
-      cout << "Invalid symbol!" << endl;
       break;
   }
 }
 
 void Brainfuck::run(){
-  cout << "Running interpreter..." << endl;
   while(instrPointer < strlen(instructionTape)){
     interpret(instructionTape[instrPointer]);
     instrPointer++;
   }
+  cout << endl;
 }
 
 int main(int argc, char* argv[]){
-  Brainfuck sth(">+++++++>++++++++++>+++>+<<<<>+++++++>++++++++++>+++>+<<<<>+++++++>++++++++++>+++>+<<<<>+++++++>++++++++++>+++>+<<<<>+++++++>++++++++++>+++>+<<<<>+++++++>++++++++++>+++>+<<<<>+++++++>++++++++++>+++>+<<<<>+++++++>++++++++++>+++>+<<<<>+++++++>++++++++++>+++>+<<<<>+++++++>++++++++++>+++>+<<<<>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
-  
-  sth.run();
-  return 0;
+  ifstream bf_file (argv[1]);
+  if (bf_file.is_open()){                 //is the argument a valid file input?
+    Brainfuck obj(bf_file);
+    obj.run();    
+    bf_file.close();
+    return 0;
+  }
+  else if (argv[1] != NULL){              //or is it a string?
+    Brainfuck obj(argv[1]);
+    obj.run();
+    return 0;
+  }
+  cerr << "Error: please provide valid input or file-name." << endl;
+  return -1;
 }
